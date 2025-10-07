12c9c1464fc491076e57519c18e801575c810f2e
marcuswh <marcuswh@andrew.cmu.edu>
2025-10-07 10:02:16 -0400

    Fixed: Users can query for name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "soar";
+    return "_soar";
   }
 
   if (query.toLowerCase().includes("andrew id")) {
